2f156311619826c29bfbc8f672cdd386da31a748
Lindsay <lindsaybolzwilson@gmail.com>
2023-04-20 18:19:37 -0400

    build books endpoint

--- app/routes.py
+++ app/routes.py
@@ -27,3 +27,27 @@ def broken_endpoint():
     response_body["hobbies"].append(new_hobby)
     return response_body
 
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+]
+
+books_bp = Blueprint("hello_world", __name__)
+
+@books_bp.route("", methods=["GET"], url_prefix="/books")
+def handle_books():
+    book_response = []
+    for book in books:
+        book_response.append({
+            'id': book.id,
+            'title': book.title,
+            'description': book.description
+        })
+    return jsonify(book_response), 200
