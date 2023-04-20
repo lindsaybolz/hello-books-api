8cd7bc084de3b4581daf6c090e91b808caab75e5
Lindsay <lindsaybolzwilson@gmail.com>
2023-04-20 17:39:05 -0400

    build hello/json endpoint

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,13 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello():
     response = "Hello World!"
     return response, 200
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    response = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    return response, 200
+
